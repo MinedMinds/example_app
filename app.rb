d827b1639670e6b802a3e1b29dfdf3688ec2f900
Amanda Laucher <akashai@hotmail.com>
2016-05-06 12:04:53 -0400

    ask about code

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
 
 get '/' do
-	"hello WV. Great to be here!"
-end 
+	erb :hello
+end
+
+post '/code' do
+	result = params[:choice]
+	if result == "yes"
+		'Awesome! Find out more at <a href="http://minedminds.org">Mined Minds</a>'
+	else
+		'Never mind. Coding is not for everyone'
+	end
+end
